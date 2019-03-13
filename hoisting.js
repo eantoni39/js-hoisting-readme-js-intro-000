b6f3a4d86640ebd2fee3b17b6c6e75bc9088ef03
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-13 02:08:14 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -14,18 +14,18 @@ var lyric = "maybe";
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
-      thisIsCrazy();
+      thisIsCrazy;
   }
 
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
+  function sayMy() {
+    var name = "Kristin"
+    console.log(name);
 
+  }
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
